3e2fd981cc68f29a801bfeff034c991a75f5c420
wihctoh <rivertoxi911@gmail.com>
2023-04-16 22:18:27 +0300

    add

--- test/src/App.js
+++ test/src/App.js
@@ -5,8 +5,19 @@ import "./styles/App.css";
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <Counter />
+      <div>
+        <div>
+          <h1>Cars catalog</h1>
+          <div>
+            <div className={styles.item}>
+              <div className={styles.img}></div>
+              <h2>Car 1</h2>
+              <p>Read more</p>
+              <button>Read more</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
