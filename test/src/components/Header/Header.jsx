5bb31bc804a8acb40943af8d9f60b7ff5bf2e78a
wihctoh <rivertoxi911@gmail.com>
2023-04-20 21:25:18 +0300

    new component

--- test/src/components/Header/Header.jsx
+++ test/src/components/Header/Header.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import "./Header.css";
+import Nav from "../Nav/Nav";
 
 const Header = () => {
   return (
     <header>
       <div className="header-logo"></div>
 
-      <nav>
-        <a href="#">
-          <p>Home</p>
-        </a>
-        <a href="#">
-          <p>Find a doctor</p>
-        </a>
-        <a href="#">
-          <p>Apps</p>
-        </a>
-        <a href="#">
-          <p>Testimonials</p>
-        </a>
-        <a href="#">
-          <p>About us</p>
-        </a>
-      </nav>
+      <Nav />
     </header>
   );
 };
---
+++ test/src/components/Nav/Nav.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href="#">
+        <p>Home</p>
+      </a>
+      <a href="#">
+        <p>Find a doctor</p>
+      </a>
+      <a href="#">
+        <p>Apps</p>
+      </a>
+      <a href="#">
+        <p>Testimonials</p>
+      </a>
+      <a href="#">
+        <p>About us</p>
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
